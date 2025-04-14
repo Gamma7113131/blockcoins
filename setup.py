bc9850f7034e1db8d351a8af10eaba03547cdbfe
Gamma7113131 <gametron2012@yahoo.com>
2025-04-14 13:25:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blockcoins',
-    version='0.1.0',
+    version='0.1.2',
     author='Vegeta',
     author_email='Gamma.scratch@gmail.com',
     description='A bot to automatically farm BlockCoins on blockcoin.vercel.app',
